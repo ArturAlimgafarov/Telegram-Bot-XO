a2d614e11fcdb04afc91b5211cb466dde1923869
Artur <artur.alimgafarov@gmail.com>
2022-07-22 03:59:49 +0500

    Start coding minimax algorithm for AI Bot

--- bot.py
+++ bot.py
@@ -4,3 +4,19 @@ import sys
 
 token = ''
 bot = telebot.TeleBot(token)
+
+board = [
+    ['_', '_', '_'],
+    ['_', '_', '_'],
+    ['_', '_', '_']
+]
+
+ai_char = 'O'
+user_char = 'X'
+ai_turn = True
+user_turn = False
+scores = {
+    user_char: -1,
+    ai_char: 1,
+    'draw': 0
+}
